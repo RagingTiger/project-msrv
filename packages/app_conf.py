e1819509cbc8e108b8e9bc96d62754f98ae8d509
RagingTiger <RagingTiger@yourmom.com>
2020-06-23 17:11:02 -0400

    Updated documentation

--- packages/app_conf.py
+++ packages/app_conf.py
@@ -6,8 +6,12 @@ from . import cryptic
 
 # custom func
 def init(app):
+    """
+        Setup initial flask app
+    """
     # get digital catalog
     with app.open_resource('config/catalog.json') as dc:
+        # add digital catalog to app config dict
         app.config['DIGI_CATALOG'] = json.load(dc)
 
     # get sha256 hash of NONCE environment variable and add to app config dic
